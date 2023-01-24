e8368354539b3fe332021b70c8e84c7c31c36a65
sridhar <sridhar@stayconnect.io>
2023-01-24 12:09:05 +0530

    ADD Put PATCH DELETE

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,11 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profile_api import serializers
 
 class HelloApiView(APIView):
     """ Test API View"""
+    serializers_class =serializers.HelloSerializer
 
-    def get(self,request,format=None):
+    def get(self, request, format=None):
         """Returns a list of APIView  features"""
         an_apiview = [
             'Uses HTTP methods as funtions (get,post,patch,put,delete)',
@@ -14,4 +17,26 @@ class HelloApiView(APIView):
             'Is mapped manually to URLs',
         ]
 
-        return Response({'message':'Hello!','an_apiview':an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+
+    def post(self,request):
+        """Create a hello message with our name"""
+        serializers = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializers.errors,status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, pk=None):
+        """Handle an update an object"""
+        return Response({'method':'put'})
+    def patch(self,request, pk=None):
+        """Handle a partial update of an objects"""
+        return({'method': 'patch'})
+    def delete(self,repones,pk=None):
+        """delete an object"""
+        return Response({'method':'DELETE'})
+        
